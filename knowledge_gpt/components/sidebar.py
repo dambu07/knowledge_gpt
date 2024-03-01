80d497e88d0ebd6a3f8ef528020260fe87463b56
dambu07 <101181320+dambu07@users.noreply.github.com>
2024-03-01 11:28:09 +0530

    Update sidebar.py

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -3,8 +3,8 @@
 # from knowledge_gpt.components.faq import faq
 
 
-# def set_openai_api_key(api_key: str):
-    # st.session_state["OPENAI_API_KEY"] = api_key
+def set_openai_api_key(api_key: str):
+    st.session_state["OPENAI_API_KEY"] = api_key
 
 
 # def sidebar():
@@ -19,9 +19,9 @@
        # api_key_input = st.text_input(
 #            "OpenAI API Key",
 #            type="password",
-#            placeholder="Paste your OpenAI API key here (sk-...)",
+           placeholder="Paste your OpenAI API key here (sk-...)",
 #            help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
-#            value=st.session_state.get("OPENAI_API_KEY", ""),
+           value=st.session_state.get("OPENAI_API_KEY", ""),
 #        )
 # 
 #        if api_key_input:
